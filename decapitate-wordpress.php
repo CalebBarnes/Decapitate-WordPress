0c245d3a49a8a3da84d5dffcb6fecf9c1ec856df
Caleb Barnes <caleb.d.barnes@gmail.com>
2021-01-21 17:51:49 -0800

    Require ACF

--- decapitate-wordpress.php
+++ decapitate-wordpress.php
@@ -8,6 +8,14 @@
  * Author URI: https://github.com/CalebBarnes
  */
 
+register_activation_hook( __FILE__, 'as8765da6sd_require_acf_plugin' );
+function as8765da6sd_require_acf_plugin(){
+
+	if (!is_plugin_active('advanced-custom-fields-pro/acf.php') && !is_plugin_active('advanced-custom-fields/acf.php') && current_user_can( 'activate_plugins' )) {
+        wp_die('Decapitate WordPress requires ACF to be installed and active. <br><a href="' . admin_url( 'plugins.php' ) . '">&laquo; Return to Plugins</a>');
+	}
+
+}
 
 if( function_exists('acf_add_options_sub_page') ) {
     acf_add_options_sub_page([
@@ -87,46 +95,55 @@ endif;
 add_action('admin_bar_menu', 'asd8i7f6a87sdf_customize_my_wp_admin_bar', 80);
 function asd8i7f6a87sdf_customize_my_wp_admin_bar($wp_admin_bar)
 {
-    $frontend_url = get_field('frontend_url', 'option');
+	if ( function_exists('get_field')) {
+		
+		$frontend_url = get_field('frontend_url', 'option');
 
-    if ($frontend_url) {
-        //Get a reference to the view-site node to modify.
-        $view_site_node = $wp_admin_bar->get_node('view-site');
-        if ($view_site_node) {
-            
-            $view_site_node->href = $frontend_url;
-            //Change target
-            $view_site_node->meta['target'] = '_blank';
-            //Update Node.
-            $wp_admin_bar->add_node($view_site_node);
-        }
-        
-        //Get a reference to the site-name node to modify.
-        $site_name_node = $wp_admin_bar->get_node('site-name');
-        if ($site_name_node) {
-            $site_name_node->href = $frontend_url;
-            //Change target
-            $site_name_node->meta['target'] = '_blank';
-            //Update Node.
-            $wp_admin_bar->add_node($site_name_node);
-        }
+		if ($frontend_url) {
+			//Get a reference to the view-site node to modify.
+			$view_site_node = $wp_admin_bar->get_node('view-site');
+			if ($view_site_node) {
+				
+				$view_site_node->href = $frontend_url;
+				//Change target
+				$view_site_node->meta['target'] = '_blank';
+				//Update Node.
+				$wp_admin_bar->add_node($view_site_node);
+			}
+			
+			//Get a reference to the site-name node to modify.
+			$site_name_node = $wp_admin_bar->get_node('site-name');
+			if ($site_name_node) {
+				$site_name_node->href = $frontend_url;
+				//Change target
+				$site_name_node->meta['target'] = '_blank';
+				//Update Node.
+				$wp_admin_bar->add_node($site_name_node);
+			}
 
-    }
+		}
+	}
 }
 
 // redirect to admin page unless preview
 add_action("template_redirect", function() {
-    $decapitate_wordpress = get_field('decapitate_wordpress', 'option');
-    if (!isset($_GET['preview']) && $decapitate_wordpress) {
-        wp_redirect('/wp-admin');
-    }
+   if ( function_exists('get_field')) {
+	    $decapitate_wordpress = get_field('decapitate_wordpress', 'option');
+		if (!isset($_GET['preview']) && $decapitate_wordpress) {
+			wp_redirect('/wp-admin');
+		}
+   }
 });
 
 function custom_frontend_url_link( $permalink, $post ) { 
-	$frontend_url = get_field('frontend_url', 'option');
-	$custom_permalink = str_replace( home_url(), $frontend_url,  $permalink );
+	if ( function_exists('get_field')) {
+		$frontend_url = get_field('frontend_url', 'option');
+		$custom_permalink = str_replace( home_url(), $frontend_url,  $permalink );
 
-	return $custom_permalink; 
+		return $custom_permalink; 
+	} else {
+		return $permalink;
+	}
 }; 
 			
 add_filter('init', 'add_frontend_url_filters');
